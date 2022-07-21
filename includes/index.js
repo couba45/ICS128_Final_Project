41e34f50fda147c181e7aed225d84a8cb53ac171
Diego <escalantediego114@gmail.com>
2022-07-21 11:23:04 -0700

    Class Catalog corrected

--- includes/index.js
+++ includes/index.js
@@ -16,7 +16,9 @@ class Catalog {
                       <div class="card">
                         <div class="p-5"><img src="${image}" class="card-img-top" alt="..." /></div>
                         <div class="card-body">
-                        <div class="price fs-3 my-5">Price: $${price}</div>
+                        <div class="price fs-3 my-5">Price: $${price.toFixed(
+                          2
+                        )}</div>
                         <h5 class="card-title">${title}</h5>
                         <p class="card-text">
                             ${description}
@@ -37,126 +39,120 @@ class Catalog {
   //delete item
 
   // add item
-
-  getCounterItem = (itemsArr, id) => {
-    let counterItem = 1;
-    for (let i = 1; i < itemsArr.length; i++) {
-      if (itemsArr[i - 1].id === id) {
-        counterItem++;
-      }
+  getTotalPrice = () => {
+    const itemsInCart = $(".price-item");
+    let totalPrice = 0;
+    for (let i = 0; i < itemsInCart.length; i++) {
+      totalPrice += parseFloat(itemsInCart[i].innerHTML.split("$")[1]);
     }
-    return counterItem;
+    $("#total-price").html("$" + totalPrice.toFixed(2));
   };
 
+  // create table
+  addButtonEventListeners = () => {
+    $("#delete-all-items").click(() => {
+      set_cookie("shopping_cart_items", {});
+      this.displayItems();
+    });
+  };
+
+  createButtons = () => {
+    $("#button-container").html(
+      `<button id="delete-all-items"class="btn btn-warning">Delete all items</button><button class="btn btn-success">Continue</button>`
+    );
+    this.addButtonEventListeners();
+  };
+
+  createTable = () => {
+    //create buttons
+
+    $("#table-container").html(` <table class="table" id="delete-item">
+                                      <thead>
+                                      <tr>
+                                          <th></th>
+                                          <th scope="col">Item</th>
+                                          <th scope="col">Qty</th>
+                                          <th scope="col">Price</th>
+                                          <th scope="col">Total</th>
+                                      </tr>
+                                      </thead>
+                                      <tbody id="table-body"></tbody>
+                                      <tfoot id="table-footer">
+                                        <th>Subtotal</th>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td id="total-price"><td>
+                                      </tfoot>
+                                  </table>
+                                  <div id="button-container" class="w-100 d-flex justify-content-between"></div>`);
+    this.createButtons();
+  };
+
+  // display items
+  displayItems = () => {
+    this.createTable();
+    let items_in_cart = get_cookie("shopping_cart_items");
+
+    let id_products = Object.keys(items_in_cart);
+    id_products.forEach((id) => {
+      console.log(items_in_cart);
+      let { title, price } = this.items_catalog[id - 1];
+      let tableContent = `<tr id="${id}">
+                            <th class="delete-element"><svg xmlns="http://www.w3.org/2000/svg" style="height: 2rem; cursor:pointer;" class="text-danger cursor-pointer" viewBox="0 0 20 20" fill="currentColor" data-item-id="${id}">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z" clip-rule="evenodd" data-item-id="${id}"/>
+                            </svg></th>
+                            <th scope="row">${title}</th>
+                            <td>${items_in_cart[id]}</td>
+                            <td>$${price}</td>
+                            <td class="price-item">$${
+                              items_in_cart[id] * price
+                            }</td>
+                        </tr>`;
+      $("#table-body").append(tableContent);
+      // event handler
+      $(".delete-element").click((event) => {
+        let itemId = event.target.getAttribute("data-item-id");
+        console.log("This is the id for delete button" + itemId);
+        let items_in_cart = get_cookie("shopping_cart_items");
+        delete items_in_cart[itemId];
+
+        set_cookie("shopping_cart_items", items_in_cart);
+        console.log(items_in_cart);
+        this.displayItems();
+        if (Object.keys(items_in_cart).length === 0) {
+          $("#dummy-text").show();
+          $("#delete-item").hide();
+          $("#button-container").addClass("d-none");
+        }
+      });
+    });
+    this.setCounter();
+    this.getTotalPrice();
+    if (Object.keys(items_in_cart).length === 0) {
+      $("#dummy-text").show();
+      $("#delete-item").hide();
+      $("#button-container").addClass("d-none");
+    }
+  };
+  setCounter = () => {
+    let itemsArr = Object.entries(get_cookie("shopping_cart_items"));
+    itemsArr = itemsArr.forEach(([key, value]) => (this.counter += value));
+    if (this.counter === 0) {
+      $("#counter").hide();
+    } else {
+      $("#counter").show();
+      $("#counter").html(this.counter);
+    }
+    this.counter = 0;
+  };
   add_event_handler() {
     // get total price
-    let getTotalPrice = () => {
-      const itemsInCart = $(".price-item");
-      let totalPrice = 0;
-      for (let i = 0; i < itemsInCart.length; i++) {
-        totalPrice += parseFloat(itemsInCart[i].innerHTML.split("$")[1]);
-      }
-      $("#total-price").html("$" + totalPrice.toFixed(2));
-    };
-
-    // create table
-    let createTable = () => {
-      let addButtonEventListeners = () => {
-        $("#delete-all-items").click(function () {
-          set_cookie("shopping_cart_items", {});
-          displayItems();
-        });
-      };
-
-      //create buttons
-      let createButtons = () => {
-        $("#button-container").html(
-          `<button id="delete-all-items"class="btn btn-warning">Delete all items</button><button class="btn btn-success">Continue</button>`
-        );
-        addButtonEventListeners();
-      };
-      $("#table-container").html(` <table class="table" id="delete-item">
-                                        <thead>
-                                        <tr>
-                                            <th></th>
-                                            <th scope="col">Item</th>
-                                            <th scope="col">Qty</th>
-                                            <th scope="col">Price</th>
-                                            <th scope="col">Total</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody id="table-body"></tbody>
-                                        <tfoot id="table-footer">
-                                          <th>Subtotal</th>
-                                          <td></td>
-                                          <td></td>
-                                          <td></td>
-                                          <td id="total-price"><td>
-                                        </tfoot>
-                                    </table>
-                                    <div id="button-container" class="w-100 d-flex justify-content-between"></div>`);
-      createButtons();
-    };
-
-    // display items
-    let displayItems = () => {
-      createTable();
-      let items_in_cart = get_cookie("shopping_cart_items");
-
-      let id_products = Object.keys(items_in_cart);
-      id_products.forEach((id) => {
-        console.log(items_in_cart);
-        let { title, price } = this.items_catalog[id - 1];
-        let tableContent = `<tr id="${id}">
-                              <th><svg xmlns="http://www.w3.org/2000/svg" style="height: 2rem; cursor:pointer;" class="text-danger delete-element cursor-pointer" viewBox="0 0 20 20" fill="currentColor" data-item-id="${id}">
-                                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z" clip-rule="evenodd" />
-                              </svg></th>
-                              <th scope="row">${title}</th>
-                              <td>${items_in_cart[id]}</td>
-                              <td>$${price}</td>
-                              <td class="price-item">$${
-                                items_in_cart[id] * price
-                              }</td>
-                          </tr>`;
-        $("#table-body").append(tableContent);
-        // event handler
-        $(".delete-element").click(function (event) {
-          let itemId = $(this).attr("data-item-id");
-          console.log("This is the id for delete button" + itemId);
-          let items_in_cart = get_cookie("shopping_cart_items");
-          delete items_in_cart[itemId];
-
-          set_cookie("shopping_cart_items", items_in_cart);
-          console.log(items_in_cart);
-          displayItems();
-          if (Object.keys(items_in_cart).length === 0) {
-            $("#dummy-text").show();
-            $("#delete-item").hide();
-            $("#button-container").addClass("d-none");
-          }
-        });
-      });
-      let itemsArr = Object.entries(get_cookie("shopping_cart_items"));
-      itemsArr = itemsArr.forEach(([key, value]) => (this.counter += value));
-      if (this.counter === 0) {
-        $("#counter").hide();
-      } else {
-        $("#counter").show();
-        $("#counter").html(this.counter);
-      }
-      this.counter = 0;
-      getTotalPrice();
-      if (Object.keys(items_in_cart).length === 0) {
-        $("#dummy-text").show();
-        $("#delete-item").hide();
-        $("#button-container").addClass("d-none");
-      }
-    };
 
     // event handler
-    $(".add-cart").click(function () {
+    $(".add-cart").click((e) => {
       $("#dummy-text").hide();
-      let product_id = $(this).attr("data-id");
+      let product_id = e.target.getAttribute("data-id");
       console.log(product_id);
       let cart_items = get_cookie("shopping_cart_items");
 
@@ -169,13 +165,16 @@ class Catalog {
       cart_items[product_id]++;
 
       set_cookie("shopping_cart_items", cart_items);
-      displayItems();
+      this.setCounter();
+    });
+    $("#view-cart").click(() => {
+      this.displayItems();
     });
   }
 
   // finished
   getCardInformation() {
-    fetch(`https://fakestoreapi.com/products`)
+    fetch(this.API_URL)
       .then((response) => {
         if (response.ok) {
           return response.json();
